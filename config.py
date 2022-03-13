a730f12a2ebaf88e93b7de05d61769bbb3a271c2
user <user@postbac.local>
2022-03-13 20:02:45 +0100

    test1

---
+++ config.py
@@ -0,0 +1,12 @@
+
+"""
+Configuration for local and deploy environments
+"""
+
+class Config:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+# Update each URL to your local and deploy URLs
+development = Config('file:///home/user/Documents/testVault/public/')
+deploy = Config('https://deployurlexample.com')
